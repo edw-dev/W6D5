925170a2ae33f0e98720cfd01c2de975eb63469f
Andrew Vitovitch <alvitovitch@gmail.com>
2021-11-12 14:49:49 -0500

    finished model validations

--- cats_99/app/models/cat.rb
+++ cats_99/app/models/cat.rb
@@ -1,6 +1,16 @@
 class Cat < ApplicationRecord
-    validates :birth_date, :color, :name, :sex, :description, presence: true
+
+    COLORS = ['red', 'blue', 'green', 'purple', 'black']
+
+    validates :birth_date, :name, :description, presence: true
+    validates :color, inclusion: COLORS
+    validates :sex, inclusion: ['M','F']
     
+    def age
+        now = Time.now.utc.to_date
+        dob = self.birth_date
+        now.year - dob.year #- ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+    end
 
 
 end
