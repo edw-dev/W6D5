e40af6079ae90167c668bc04e157e6312e63f0b3
Andrew Vitovitch <alvitovitch@gmail.com>
2021-11-12 16:28:06 -0500

    finished most of index/show

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -1,7 +1,16 @@
-class cat_controller < ApplicationController
+class CatsController < ApplicationController
     def index
-        cats = Cat.all
-        render json: cats
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find_by(id: params[:id])
+        if @cat
+            render :show
+        else
+            redirect_to cats_url
+        end
     end
 
 
